0390c8d64633ea347c0fd4b167b6ab5693e0384b
akhilch492 <akhilchoudarapally@gmail.com>
2018-04-19 14:27:59 +0530

    Add files via upload

---
+++ CommonElements.java
@@ -0,0 +1,23 @@
+class CommonElements
+{
+public static void main(String[] args)
+{
+int a1[][]={{1,2,3},{1,3,4}};
+int a2[][]=new int[2][3];
+
+for(int i=0;i<a1.length;i++)
+{
+for(int j=0;j<a1.length;j++)
+{
+if(a2[i]==a2[j])
+{
+
+
+
+System.out.println(a1[i][j]);
+}
+
+}
+}
+}
+}
